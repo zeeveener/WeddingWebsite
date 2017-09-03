ec3fc68d98306438a2bc424a4b8a0975bb7bf966
Zac Beemer <zeeveener@gmail.com>
2017-09-02 21:08:52 -0600

    Add php script to handle submission of form

---
+++ actions/submit.php
@@ -0,0 +1,20 @@
+<?php
+const DB_HOST = "192.168.99.100";
+const DB_PORT = 3306;
+const DB_USER = "zac_wedding";
+const DB_PASS = "budgie";
+const DB_DB = "wedding2018";
+const DB_RSVP_TABLE = "RSVP";
+
+$firstName = $_POST["first-name"];
+$lastName = $_POST["last-name"];
+$email = $_POST["email"];
+$coming = isset($_POST["coming"]);
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DB, DB_PORT);
+$statement = $connection->prepare("INSERT INTO RSVP(first_name, last_name, email, coming) VALUES(?, ?, ?, ?)");
+$statement->bind_param("sssi", $firstName, $lastName, $email, intval($coming));
+$statement->execute();
+$statement->close();
+
+header("Location: /html/index.php?responded=1#rsvp");
